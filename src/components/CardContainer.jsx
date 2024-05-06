9c32b77600b2039a9ad352741b2868cdf71045f7
HIMANSHUYELANE <yelanehimanshu@gmail.com>
2024-05-06 13:41:27 +0530

    fix API

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
 import CountryCard from "./CountryCard";
-import countriesData from "../../countriesData";
+// import countriesData from "../../countriesData";
 
 export default function CardContainer({query,select}) {
+  const [countriesData, setCountriesData] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesData(data)
+    })
+  },[])
   return (
     <div className="flex justify-evenly flex-wrap">
       {countriesData
@@ -15,7 +23,7 @@ export default function CardContainer({query,select}) {
           return (
             <CountryCard
               key={i}
-              name={country.name.official}
+              name={country.name.common}
               flag={country.flags.png}
               population={country.population}
               region={country.region}
